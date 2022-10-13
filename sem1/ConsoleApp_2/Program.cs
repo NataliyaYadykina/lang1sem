be63ca12fd91e750e0bdb2bd30df7344daf36bc6
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-13 22:23:18 +0300

    Add ConsoleApp_2

---
+++ sem1/ConsoleApp_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int sqrNum2 = num2 * num2;
+
+if(num1 == sqrNum2)
+{
+    Console.WriteLine("Первое число является квадратом второго!");
+}
+else
+{
+    Console.WriteLine("Первое число Не является квадратом второго!");
+}
