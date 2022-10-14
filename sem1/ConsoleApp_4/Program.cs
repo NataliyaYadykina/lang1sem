7a9a7807f0cd91621da4c2fa57afd1711ed1f056
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-14 09:15:25 +0300

    Add ConsoleApp_4

---
+++ sem1/ConsoleApp_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n <= 0)
+{
+    Console.Write("Введите положительное число N: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+int negativeN = n * -1;
+while(negativeN <= n)
+{
+    Console.WriteLine(negativeN);
+    negativeN = negativeN +1;
+}
