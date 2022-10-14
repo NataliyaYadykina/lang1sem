7e6747ca9898bcefbabbb142e189a24137d75529
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-14 09:20:40 +0300

    Changed ConsoleApp_3

--- sem1/ConsoleApp_3/Program.cs
+++ sem1/ConsoleApp_3/Program.cs
@@ -1,35 +1,11 @@
 ﻿Console.Write("Введите номер дня недели: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number < 1 || number > 7)
-{
-    Console.WriteLine("Введен неверный номер дня недели!");
-}
-if(number == 1)
-{
-    Console.WriteLine("Понедельник");
-}
-if(number == 2)
-{
-    Console.WriteLine("Вторник");
-}
-if(number == 3)
-{
-    Console.WriteLine("Среда");
-}
-if(number == 4)
-{
-    Console.WriteLine("Четверг");
-}
-if(number == 5)
-{
-    Console.WriteLine("Пятница");
-}
-if(number == 6)
-{
-    Console.WriteLine("Суббота");
-}
-if(number == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
+if(number < 1 || number > 7) Console.WriteLine("Введен неверный номер дня недели!");
+if(number == 1) Console.WriteLine("Понедельник");
+if(number == 2) Console.WriteLine("Вторник");
+if(number == 3) Console.WriteLine("Среда");
+if(number == 4) Console.WriteLine("Четверг");
+if(number == 5) Console.WriteLine("Пятница");
+if(number == 6) Console.WriteLine("Суббота");
+if(number == 7) Console.WriteLine("Воскресенье");
