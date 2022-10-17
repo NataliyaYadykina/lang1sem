398151611c6784c29be3c678a59608fe8b338afa
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-17 21:09:27 +0300

    Add ConsoleApp_02

---
+++ sem2/ConsoleApp_02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и
+//удаляет вторую цифру этого числа.
+//
+
+// Случайное число
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int a1 = number / 100;
+Console.WriteLine(a1);
+int a2 = number % 10;
+Console.WriteLine(a2);
+int result = a1 * 10 + a2;
+Console.WriteLine(result);
