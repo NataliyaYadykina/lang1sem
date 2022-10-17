c756bb87a33d25a4741af66bd705a772da52d309
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-17 21:18:14 +0300

    Add ConsoleApp_03

---
+++ sem2/ConsoleApp_03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Vvedi A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedi B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a % b == 0)
+{
+    Console.WriteLine("Kratno");
+}
+else
+{
+    int ost = a % b;
+    Console.WriteLine($"Ostatok raven {ost}.");
+}
