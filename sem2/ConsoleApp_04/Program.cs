6768d156254670ed4c1d1bd8802efa0af31fff7d
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-17 21:28:42 +0300

    Add ConsoleApp_04

---
+++ sem2/ConsoleApp_04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+//
+
+Console.Write("Vvedi chislo: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if(a % 7 == 0 && a % 23 == 0) Console.WriteLine("Kratno");
+else
+{
+    Console.WriteLine("Ne kratno!");
+}
