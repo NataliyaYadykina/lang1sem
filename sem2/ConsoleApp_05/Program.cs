a20a7798f874a7e5d4f87f27aa9d26d62c8e4576
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-17 21:38:25 +0300

    Add ConsoleApp_05

---
+++ sem2/ConsoleApp_05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Vvedi A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedi B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if(a == b * b || b == a * a) Console.WriteLine("Odno kvadrat drugogo");
+else Console.WriteLine("Net!");
