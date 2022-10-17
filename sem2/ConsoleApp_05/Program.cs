d03086f0515edaab7093938d32238b7855902a20
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-17 22:38:30 +0300

    Change ConsoleApp_05

--- sem2/ConsoleApp_05/Program.cs
+++ sem2/ConsoleApp_05/Program.cs
@@ -5,5 +5,9 @@ Console.Write("Vvedi B: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 
-if(a == b * b || b == a * a) Console.WriteLine("Odno kvadrat drugogo");
-else Console.WriteLine("Net!");
+if(a != b * b && b != a * a) Console.WriteLine("Никакое число не является квадратом другого.");
+else
+{
+    if(a == b * b) Console.WriteLine($"{a} является квадратом числа {b}.");
+    else Console.WriteLine($"{b} является квадратом числа {a}.");
+}
