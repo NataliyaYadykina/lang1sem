4d3102c05fbe9f2d7c448140f233e333fec61f3a
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-20 22:55:53 +0300

    add consoleapp 04

---
+++ sem3/ConsoleApp_04/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+// Получаем таблицу квадратов
+void PrintSquaresToConsole(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i);
+    }
+}
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintSquaresToConsole(n);
