f40c9da240f9c82a6d89ba2f8d4900aa718b6571
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-20 21:52:50 +0300

    Add ConsoleApp 02

---
+++ sem3/ConsoleApp_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Получить номер координатной четверти
+string GetDiapason(int pointArea)
+{
+    string diapason = "Неверный номер четверти. Попробуйте снова.";
+    if(pointArea == 1) diapason = "x > 0, y > 0";
+    if(pointArea == 2) diapason = "x < 0, y > 0";
+    if(pointArea == 3) diapason = "x < 0, y < 0";
+    if(pointArea == 4) diapason = "x > 0, y < 0";
+    return diapason;
+}
+
+Console.Write("Введите номер координатной четверти: ");
+int pointArea = Convert.ToInt32(Console.ReadLine());
+
+string diapason = GetDiapason(pointArea);
+Console.WriteLine(diapason);
