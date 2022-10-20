6719dabd6c205ea118aa62bfe48988e70551bb51
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-10-20 19:38:04 +0300

    Add source

---
+++ source/part_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// print one line
+Console.Write("Hello, World!");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// print other line
+Console.WriteLine("Hello, World!");
+
+// variables
+int a = 0;
+int b = 0;
+int i = 0;
+int count = 5;
+
+// random number
+int number = new Random().Next(1, 10);
+
+// if else mini
+if(a == b) Console.WriteLine("a = b");
+else Console.WriteLine("a != b");
+
+// if else full
+if(a == b)
+{
+    Console.WriteLine("a = b");
+}
+else
+{
+    Console.WriteLine("a != b");
+}
+
+// while
+while(i < count)
+{
+    Console.WriteLine(i);
+    i++;
+}
