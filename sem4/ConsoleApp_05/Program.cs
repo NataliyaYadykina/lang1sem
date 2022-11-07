b1b15bf43c9b38c675040e3d2d83f11195de73de
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-07 22:50:47 +0300

    Add ConsoleApp 05

---
+++ sem4/ConsoleApp_05/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+// Console.WriteLine("[{0}]", string.Join(", ", array));
+
+
+void PrintArr(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void GetArray(int[] array)
+{
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+}
+
+int[] arr = new int[8];
+
+PrintArr(arr);
+GetArray(arr);
+PrintArr(arr);
