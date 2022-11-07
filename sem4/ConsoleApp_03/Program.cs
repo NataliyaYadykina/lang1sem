e95b51e1bb27e270f3a57cdb25533762e00a0869
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-07 21:34:08 +0300

    Add ConsoleApp 03

---
+++ sem4/ConsoleApp_03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+int NumLength(int number)
+{
+    string strNum = Math.Abs(number).ToString();
+    int length = strNum.Length;
+    return length;
+}
+
+//int num = -12345;
+//string strNum = Math.Abs(num).ToString();
+//Console.WriteLine(strNum.Length);
+
+
+Console.Write("Write number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumLength(a));
