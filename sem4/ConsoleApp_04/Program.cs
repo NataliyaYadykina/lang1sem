40d15f243b519add789eed7600ef079118abe0ed
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-07 21:39:54 +0300

    Add ConsoleApp 04

---
+++ sem4/ConsoleApp_04/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+
+int GetFactorial(int number)
+{
+    int res = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+
+//int num = -12345;
+//string strNum = Math.Abs(num).ToString();
+//Console.WriteLine(strNum.Length);
+
+
+Console.Write("Write number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetFactorial(a));
+
