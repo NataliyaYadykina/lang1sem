7d3b42fd19f030205cbc9d31c06f2a7da333915f
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-07 21:14:14 +0300

    Add ConsoleApp 02

---
+++ sem4/ConsoleApp_02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+int GetSum(int number)
+{
+    int sum = 0;
+
+    /* int tempValue = 1;
+    while(tempValue <= number)
+    {
+        sum = sum + tempValue;
+        tempValue++;
+    } */
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+Console.Write("Write number A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = GetSum(number);
+Console.WriteLine(sum);
