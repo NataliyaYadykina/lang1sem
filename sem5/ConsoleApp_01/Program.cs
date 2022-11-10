4331d4052f59e4fe1bb139f6a8410239abb33306
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-10 20:33:50 +0300

    add consolepp 01

---
+++ sem5/ConsoleApp_01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив из 12 элементов,
+// заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму положительных и сумму отрицательных элементов массива.
+
+
+int[] array = new int[12];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] + " ");
+}
+
+int sumPositive = 0;
+int sumNegative = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        sumPositive += array[i];
+    } 
+    else
+    {
+        sumNegative += array[i];
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных = {sumPositive}, сумма отрицательных = {sumNegative}");
