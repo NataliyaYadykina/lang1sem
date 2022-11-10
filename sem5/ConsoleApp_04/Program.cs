52515cbd7f8d5f4cf852a8d055819d7b7d2bb17b
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-10 21:26:29 +0300

    Add ConsoleApp 04

---
+++ sem5/ConsoleApp_04/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int CountElements(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = new int[12];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 250);
+    Console.Write(array[i] + " ");
+}
+
+int count = CountElements(array);
+Console.WriteLine();
+Console.WriteLine(count);
