905a815d36dd71408389073c500745540b87496e
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-14 21:59:31 +0300

    Add ConsoleApp 03

---
+++ sem6/ConsoleApp_03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string ConvertNum(int number)
+{
+    string res = string.Empty;
+    while (number > 0)
+    {
+        res = Convert.ToString(number % 2) + res;
+        number /= 2;
+    }
+    return res;
+}
+
+int number = new Random().Next(1,100);
+Console.WriteLine(number);
+Console.WriteLine(ConvertNum(number));
