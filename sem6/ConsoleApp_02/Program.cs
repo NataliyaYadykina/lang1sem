7a4c852632589a3aa6bffdd1e87e355c371471bc
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-14 21:25:20 +0300

    Add ConsoleApp 02

---
+++ sem6/ConsoleApp_02/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+ * может ли существовать треугольник с сторонами такой длины.
+ Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.*/
+
+bool isTriangle(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b) return true;
+    else return false;
+}
+
+Console.Write("Write a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(isTriangle(a, b, c));
