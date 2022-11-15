a13de49cc9ad234d1403e293c23df02624ad45e4
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-15 12:05:15 +0300

    Add ConsoleApp 05 CopyArray

---
+++ sem6/ConsoleApp_05/Program.cs
@@ -0,0 +1,42 @@
+﻿// 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void PrintArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] RandomFillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 250);
+        //Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i] + 1;
+    }
+    return copyArray;
+}
+
+Console.Write("Set length of array: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[number];
+
+RandomFillArray(arr);
+Console.Write("First array: ");
+PrintArr(arr);
+int[] copyArr = CopyArray(arr);
+Console.Write("Copy array: ");
+PrintArr(copyArr);
