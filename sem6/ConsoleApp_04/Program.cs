a27db0afad843ab94a0518d56a6716b367360439
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-15 11:31:49 +0300

    Add ConsoleApp 04 Fibonacci

---
+++ sem6/ConsoleApp_04/Program.cs
@@ -0,0 +1,33 @@
+﻿// 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5-> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+void PrintArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] Fibonacci(int[] array)
+{
+    array[0] = 0;
+    array[1] = 1;
+    for(int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+Console.Write("Write number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[number];
+PrintArr(arr);
+Fibonacci(arr);
+PrintArr(arr);
