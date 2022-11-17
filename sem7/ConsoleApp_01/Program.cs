48064a745d4e956e6cf2a775a592bc89891cf9d1
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-17 20:42:54 +0300

    Add ConsoleApp 01 Fill Array[m, n]

---
+++ sem7/ConsoleApp_01/Program.cs
@@ -0,0 +1,25 @@
+﻿// задайте двумерный массив размером m*n, 
+// заполненный случайными числами
+
+void FillArray(int[,] array, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(-100, 101);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Write m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+FillArray(array, m, n);
