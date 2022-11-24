c40fe517e87c2044d2045c464863bdaa8d3fd441
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-24 21:44:19 +0300

    Add ConsoleApp 04 - Sum Symbols in Number

---
+++ sem9/ConsoleApp_04/Program.cs
@@ -0,0 +1,18 @@
+﻿// 67: Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр.
+// 453-> 12
+// 45-> 9
+
+int PrintSumSymbolsNumber(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return n % 10 + PrintSumSymbolsNumber(n / 10);
+}
+
+Console.Write("Write N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintSumSymbolsNumber(n));
