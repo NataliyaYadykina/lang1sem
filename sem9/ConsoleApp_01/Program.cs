79d66fcdf136a1a152a57c92e3bcd345e5a09971
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-24 20:39:50 +0300

    Add ConsoleApp 01 - Factorial Recursive

---
+++ sem9/ConsoleApp_01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пример рекурсии - нахождение факториала.
+
+int FactorialFor(int n)
+{
+    int res = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        res *= i;
+    }
+    return res;
+}
+
+int FactorialR(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return n * FactorialR(n - 1);
+}
+
+Console.Write("Write N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FactorialR(n));
