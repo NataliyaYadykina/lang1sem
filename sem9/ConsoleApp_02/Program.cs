573b3fb0a2730ba32d93e1912fef4142ac67f40e
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-11-24 20:50:39 +0300

    Add ConsoleApp 02 - Print Numbers from N to 1

---
+++ sem9/ConsoleApp_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// 63: Задайте значение N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от 5 до 1.
+// N = 5-> "5, 4, 3, 2, 1"
+// N = 6-> "6, 5, 4, 3, 2, 1"
+
+int PrintNumbers(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int tempResult = n;
+    Console.WriteLine(tempResult);
+    return PrintNumbers(n - 1);
+}
+
+Console.Write("Write N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(n));
